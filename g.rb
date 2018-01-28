462e1dcdc088554b91766c6e1bfe40ff3d9c649c
Paul McKellar <paul.mckellar@gmail.com>
2018-01-28 11:23:21 -0800

    catr

--- g.rb
+++ g.rb
@@ -2,18 +2,21 @@ DEBUG = true
 PATH = "../litecoin/"
 BINARY = "src/litecoin-cli"
 
-def command(line)
-  puts line if DEBUG
-  result = `#{line}`
+def run_command(c)
+  puts c if DEBUG
+  result = `#{c}`
   puts result if DEBUG
 end
 
-command("#{PATH}#{BINARY} getinfo")
+def run_litecoin_command(c)
+  run_command("#{PATH}#{BINARY} #{c}")
+end
 
 commands = File.new("commands", "r")
 commands.each do |line|
   line = line.rstrip
   next if line[0] == '='
   next if line == ""
-  puts line
+  command = line.split(" ")[0]
+  run_litecoin_command(command)
 end
