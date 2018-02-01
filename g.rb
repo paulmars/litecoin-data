be3f40b9572fd7f0cc0f93a73ffc9d2acd8b4b72
Paul McKellar <paul.mckellar@gmail.com>
2018-02-01 09:41:12 -0800

    get block

--- g.rb
+++ g.rb
@@ -25,4 +25,15 @@ def best_header
   lite_cmd("getblockheader #{best_hash} true")
 end
 
+def best_block
+  lite_cmd("getblock #{best_hash} true")
+end
 
+def block(hash, verbose)
+  lite_cmd("getblock \"#{best_hash}\" #{verbose}")
+end
+
+# ./src/litecoin-cli getblock 9e139098833a67e2fb58f76f08d84ccda7cde5170060d8d21083fedd91e52b70 1
+# ./src/litecoin-cli getblock 9e139098833a67e2fb58f76f08d84ccda7cde5170060d8d21083fedd91e52b70 2
+
+first_block = block(best_hash, 2)
